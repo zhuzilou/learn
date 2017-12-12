fca5f294caf2eef074d96f8ed97684bdb40ba859
zyw <zhangyx@dxinfor.com>
2017-12-12 15:07:40 +0800

    声明事事务：讲的略简单，注意配合@Transactional中的属性值使用。

---
+++ ch8_4/src/main/java/cc/lostyouth/spring/ch8_4/domain/Person.java
@@ -0,0 +1,76 @@
+package cc.lostyouth.spring.ch8_4.domain;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by endless on 12/12/2017.
+ */
+@Entity
+public class Person {
+    @GenericGenerator(
+            name = "SEQ_PERSON",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                    @Parameter(name = "sequence_name", value = "SEQ_PERSON")
+            }
+    )
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_PERSON")
+    private Long id;
+
+    private String name;
+
+    private Integer age;
+
+    private String address;
+
+    public Person() {
+        super();
+    }
+
+    public Person(Long id, String name, String address, Integer age) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
